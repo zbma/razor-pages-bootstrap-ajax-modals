12195ab3fc425aeb6fa0604a5d9e5a80d52266ce
Zbigniew <zbigniew@solidcodesoftware.com>
2019-06-09 14:31:59 +0200

    Initial commit.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using RazorPagesAjaxModals.Models;
+using System.Collections.Generic;
+
+namespace RazorPagesAjaxModals.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly static List<Contact> Contacts = new List<Contact>();
+
+        public void OnGet()
+        {
+
+        }
+
+        public PartialViewResult OnGetContactModalPartial()
+        {
+            return new PartialViewResult
+            {
+                ViewName = "_ContactModalPartial",
+                ViewData = new ViewDataDictionary<Contact>(ViewData, new Contact { })
+            };
+        }
+
+        public PartialViewResult OnPostContactModalPartial(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                Contacts.Add(model);
+            }
+
+            return new PartialViewResult
+            {
+                ViewName = "_ContactModalPartial",
+                ViewData = new ViewDataDictionary<Contact>(ViewData, model)
+            };
+        }
+    }
+}
